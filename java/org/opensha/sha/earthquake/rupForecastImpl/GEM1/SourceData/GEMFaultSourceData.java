e0bfd3b30e4c68465aa44bab70c74ec1267f3660
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-11-18 15:45:42 +0100

    Added equals implementation for GEMFaultSourceData

--- java/org/opensha/sha/earthquake/rupForecastImpl/GEM1/SourceData/GEMFaultSourceData.java
+++ java/org/opensha/sha/earthquake/rupForecastImpl/GEM1/SourceData/GEMFaultSourceData.java
@@ -2,16 +2,13 @@ package org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData;
 
 import java.io.Serializable;
 
-import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMSourceData;
 import org.opensha.sha.faultSurface.FaultTrace;
 import org.opensha.sha.magdist.IncrementalMagFreqDist;
 import org.opensha.sha.util.TectonicRegionType;
 
-public class GEMFaultSourceData extends GEMSourceData implements Serializable {
+public class GEMFaultSourceData extends GEMSourceData implements Serializable
+{
 
-    /**
-	 * 
-	 */
     private static final long serialVersionUID = 1L;
 
     private FaultTrace trace;
@@ -22,24 +19,15 @@ public class GEMFaultSourceData extends GEMSourceData implements Serializable {
     private IncrementalMagFreqDist mfd;
     private boolean floatRuptureFlag;
 
-    /**
-	 * 
-	 */
-    public GEMFaultSourceData() {
+    public GEMFaultSourceData()
+    {
     }
 
-    /**
-     * 
-     * @param trc
-     * @param dip
-     * @param rake
-     * @param sdepLo
-     * @param sdepUp
-     */
     public GEMFaultSourceData(String id, String name,
             TectonicRegionType tectReg, IncrementalMagFreqDist mfd,
             FaultTrace trc, double dip, double rake, double seismDepthLow,
-            double seismDepthUpp, boolean floatRuptureFlag) {
+            double seismDepthUpp, boolean floatRuptureFlag)
+    {
         this.id = id;
         this.name = name;
         this.tectReg = tectReg;
@@ -52,32 +40,57 @@ public class GEMFaultSourceData extends GEMSourceData implements Serializable {
         this.floatRuptureFlag = floatRuptureFlag;
     }
 
-    public double getDip() {
+    public double getDip()
+    {
         return dip;
     }
 
-    public double getRake() {
+    public double getRake()
+    {
         return rake;
     }
 
-    public double getSeismDepthLow() {
+    public double getSeismDepthLow()
+    {
         return seismDepthLow;
     }
 
-    public double getSeismDepthUpp() {
+    public double getSeismDepthUpp()
+    {
         return seismDepthUpp;
     }
 
-    public IncrementalMagFreqDist getMfd() {
+    public IncrementalMagFreqDist getMfd()
+    {
         return mfd;
     }
 
-    public FaultTrace getTrace() {
+    public FaultTrace getTrace()
+    {
         return this.trace;
     }
 
-    public boolean getFloatRuptureFlag() {
+    public boolean getFloatRuptureFlag()
+    {
         return floatRuptureFlag;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof GEMFaultSourceData))
+        {
+            return false;
+        }
+
+        GEMFaultSourceData other = (GEMFaultSourceData) obj;
+
+        return id.equals(other.id) && name.equals(other.name)
+                && tectReg.equals(other.tectReg)
+                && mfd.equals((Object) other.mfd) && trace.equals(other.trace)
+                && dip == other.dip && rake == other.rake
+                && seismDepthLow == other.seismDepthLow
+                && seismDepthUpp == other.seismDepthUpp;
     }
 
 }
